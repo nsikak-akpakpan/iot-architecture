06e740bd477119ed8e61b96b96f2071da1c12499
Nsikak Akpakpan <36205879+nsikak-akpakpan@users.noreply.github.com>
2019-06-12 14:39:32 -0500

    Create sample.c

---
+++ azure/scripts/sample.c
@@ -0,0 +1,9 @@
+try
+{
+    CloudJob job = batchClient.JobOperations.CreateJob();
+    job.Id = JobId;
+    job.PoolInformation = new PoolInformation { PoolId = PoolId };
+
+    job.Commit();
+}
+...
